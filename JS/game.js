29cf9a8b59d10a00f44a5ed2e3da91852fec5845
Alane-Tc <alantamez28@hotmail.com>
2020-10-06 13:44:08 -0500

    Se modificarion las frases del arreglo phrases

--- JS/game.js
+++ JS/game.js
@@ -5,12 +5,12 @@ window.addEventListener("load", function () {
     var div_message = document.querySelector("#parrafo");
 
     const phrases = [
-        "Titulo Ramdom 1", "Titulo Ramdom 2", "Titulo Ramdom 3", "Titulo Ramdom 4",
-        "Titulo Ramdom 5", "Titulo Ramdom 6 ", "Titulo Ramdom 7", "Titulo Ramdom 8",
-        "Titulo Ramdom 9", "Titulo Ramdom 10"];
+        "Que grande", "Felicidades crack", "GG", "Good Game",
+        "Ahuevo ganaste", "Juega otra culo ", "Crack", "WOW",
+        " Quede Impactado", "No puede ser =("];
 
     const answer = ["Ya perdiste jajaja", "Sigue intentado", "Qué pendejo estas", "Vulve a intentarlo", "JAJAJAJAJA",
-    "¿Qué?, ¿No puedes ganar?", "Eres un perdedor", "Ya rindete joder", "Pinche chairo estúpido", "F en el chat shavales"];
+    "¿Qué?, ¿No puedes ganar?", "Eres un perdedor", "Pinche cara de mi miembro", "Pinche chairo estúpido", "F en el chat shavales"];
 
     document.querySelector("#Resu_division").innerHTML = "0" + " Veces";
     function Contando() {
@@ -24,7 +24,7 @@ window.addEventListener("load", function () {
             "<a href=index.html><button type=button id= Game_Over>Rendirse</button></a>";
         }
         if (counter == 50) {//GANAR 
-            div_message.innerHTML = phrases[Math.floor(Math.random() * phrases.length)] + "<br><a href=juego.html><button type=button id= Reinicio>Felicidades, juega de nuevo culo</button></a>";
+            div_message.innerHTML = phrases[Math.floor(Math.random() * phrases.length)] + "<br><a href=juego.html><button type=button id= End>Felicidades, juega de nuevo culo</button></a>";
             button.disabled = true;
             button.style.background = "#ccc";
         }    
