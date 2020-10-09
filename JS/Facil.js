0d4ced2bf41459d72d7ce7a5747409147c851b80
Alane-Tc <alantamez28@hotmail.com>
2020-10-09 14:36:56 -0500

    Se agrego mensaje de que nivel estas

--- JS/Facil.js
+++ JS/Facil.js
@@ -10,7 +10,7 @@ window.addEventListener("load", function () {
         " Quede Impactado", "No puede ser =("];*/
 
     const answer = ["Ya perdiste jajaja", "Sigue intentado", "Qué pendejo estas", "Vulve a intentarlo", "JAJAJAJAJA",
-    "¿Qué?, ¿No puedes ganar?", "Eres un perdedor", "Pinche cara de mi miembro", "Pinche chairo estúpido", "F en el chat shavales"];
+        "¿Qué?, ¿No puedes ganar?", "Eres un perdedor", "Pinche cara de mi miembro", "Pinche chairo estúpido", "F en el chat shavales"];
 
     var randomImage = new Array();
     randomImage[0] = "img/Img_Win/win1.jpg";
@@ -23,29 +23,34 @@ window.addEventListener("load", function () {
     randomImage[7] = "img/Img_Win/win8.jpg";
     randomImage[8] = "img/Img_Win/win9.PNG";
     randomImage[9] = "img/Img_Win/win10.jpg";
-    
-    function getRandomImage() { 
-        var number = Math.floor(Math.random()*randomImage.length);
-        div_message.innerHTML=('<a href=index.html><img id=generator title="Click aqui" src="'+randomImage[number]+'" /></a>');
-        }
 
+    function getRandomImage() {
+        var number = Math.floor(Math.random() * randomImage.length);
+        div_message.innerHTML = ('<a href=index.html><img id=generator title="Click aqui" src="' + randomImage[number] + '" /></a>');
+    }
+    div_message.innerHTML = "Estas en el nivel Fácil";
     document.querySelector("#Resu_division").innerHTML = "0" + " Veces";
     function Contando() {
         var aleatorio = Math.round(Math.random() * 20);
         counter++;
         document.querySelector("#Resu_division").innerHTML = counter + " Veces";
+
+        if (counter == 1) {//Quitar el mensaje
+            div_message.innerHTML = message_white;
+        }
+
         if (aleatorio == 5) { //Bloquear 
             button.style.background = "#ccc";
             button.disabled = true;
-            div_message.innerHTML = answer[Math.floor(Math.random() * answer.length)] + "<br><a href=juego.html><button type=button id= Reinicio>Reiniciar</button></a>"+
-            "<a href=index.html><button type=button id= Game_Over>Rendirse</button></a>";
+            div_message.innerHTML = answer[Math.floor(Math.random() * answer.length)] + "<br><a href=juego.html><button type=button id= Reinicio>Reiniciar</button></a>" +
+                "<a href=index.html><button type=button id= Game_Over>Rendirse</button></a>";
         }
         if (counter == 50) {//GANAR 
-           // div_message.innerHTML = phrases[Math.floor(Math.random() * phrases.length)] + "<br><a href=index.html><button type=button id= End>Felicidades</button></a>";
-           getRandomImage();
+            // div_message.innerHTML = phrases[Math.floor(Math.random() * phrases.length)] + "<br><a href=index.html><button type=button id= End>Felicidades</button></a>";
+            getRandomImage();
             button.disabled = true;
             button.style.background = "#ccc";
-        }    
+        }
     }
     button.addEventListener("click", function () {
         Contando();
