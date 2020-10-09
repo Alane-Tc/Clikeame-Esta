50901d1883eb20efc1e7d1e936a19b2351d6aed6
Alane-Tc <alantamez28@hotmail.com>
2020-10-09 14:22:39 -0500

    Se agregaron imagenes en el arreglo

--- JS/Dificil.js
+++ JS/Dificil.js
@@ -1,15 +1,27 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     let counter = 0;
     var button = document.querySelector("#Boton_Click");
     var div_message = document.querySelector("#parrafo");
 
-    const phrases = [
-        "Que grande", "Felicidades crack", "GG", "Good Game",
-        "Ahuevo ganaste", "Juega otra culo ", "Crack", "WOW",
-        " Quede Impactado", "No puede ser =("];
-
     const answer = ["Ya perdiste jajaja", "Sigue intentado", "Qué pendejo estas", "Vulve a intentarlo", "JAJAJAJAJA",
         "¿Qué?, ¿No puedes ganar?", "Eres un perdedor", "Pinche cara de mi miembro", "Pinche chairo estúpido", "F en el chat shavales"];
+
+    var randomImage = new Array();
+    randomImage[0] = "img/Img_Win/win1.jpg";
+    randomImage[1] = "img/Img_Win/win2.jpeg";
+    randomImage[2] = "img/Img_Win/win3.jpg";
+    randomImage[3] = "img/Img_Win/win4.jpg";
+    randomImage[4] = "img/Img_Win/win5.jpg";
+    randomImage[5] = "img/Img_Win/win6.jpg";
+    randomImage[6] = "img/Img_Win/win7.jpg";
+    randomImage[7] = "img/Img_Win/win8.jpg";
+    randomImage[8] = "img/Img_Win/win9.PNG";
+    randomImage[9] = "img/Img_Win/win10.jpg";
+
+    function getRandomImage() {
+        var number = Math.floor(Math.random() * randomImage.length);
+        div_message.innerHTML = ('<a href=index.html><img id=generator title="Click aqui" src="' + randomImage[number] + '" /></a>');
+    }
 
     document.querySelector("#Resu_division").innerHTML = "0" + " Veces";
     function Contando() {
@@ -20,25 +32,25 @@ window.addEventListener("load", function(){
             button.style.background = "#ccc";
             button.disabled = true;
             div_message.innerHTML = answer[Math.floor(Math.random() * answer.length)] + "<br><a href=Dificil.html><button type=button id= Reinicio>Reiniciar</button></a>" +
-            "<a href=index.html><button type=button id= Game_Over>Rendirse</button></a>";
+                "<a href=index.html><button type=button id= Game_Over>Rendirse</button></a>";
         }
 
         if (aleatorio == 15) { //Bloquear 
             button.style.background = "#ccc";
             button.disabled = true;
             div_message.innerHTML = answer[Math.floor(Math.random() * answer.length)] + "<br><a href=Dificil.html><button type=button id= Reinicio>Reiniciar</button></a>" +
-            "<a href=index.html><button type=button id= Game_Over>Rendirse</button></a>";
+                "<a href=index.html><button type=button id= Game_Over>Rendirse</button></a>";
         }
 
         if (aleatorio == 25) { //Bloquear 
             button.style.background = "#ccc";
             button.disabled = true;
             div_message.innerHTML = answer[Math.floor(Math.random() * answer.length)] + "<br><a href=Dificil.html><button type=button id= Reinicio>Reiniciar</button></a>" +
-            "<a href=index.html><button type=button id= Game_Over>Rendirse</button></a>";
+                "<a href=index.html><button type=button id= Game_Over>Rendirse</button></a>";
         }
 
         if (counter == 50) {//GANAR 
-            div_message.innerHTML = phrases[Math.floor(Math.random() * phrases.length)] + "<br><a href=index.html><button type=button id= End>Felicidades</button></a>";
+            getRandomImage();
             button.disabled = true;
             button.style.background = "#ccc";
         }
